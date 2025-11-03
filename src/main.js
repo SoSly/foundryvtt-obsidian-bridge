a9886b1d5a9879a621fae6a89cc4c714a21143f1
Kevin Kragenbrink <kevin@writh.net>
2025-11-02 21:25:21 -0800

    refactor: move infrastructure files out of interface layer
    Relocates registerHandlebarsHelpers.js and registerHooks.js from
    src/interface/import/ to src/infrastructure/ to better align with
    clean architecture principles. These files handle framework setup
    and initialization, not interface-specific logic.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { registerImportHooks } from './interface/import/registerHooks.js';
+import { registerImportHooks } from './infrastructure/registerHooks.js';
 
 Hooks.once('init', async () => {
     console.log('Obsidian Bridge | Initializing');
